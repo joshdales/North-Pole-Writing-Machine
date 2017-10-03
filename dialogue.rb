f1b4a85f1fa0113201047045e87a3da19ef6eb9c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 12:53:24 -0400

    create ERB.new

--- dialogue.rb
+++ dialogue.rb
@@ -2,12 +2,13 @@ require 'erb'
 
 price = 8.75
 
-line_1 = "Bill:   That will cost you $8.75 plus taxes, ma'am."
+line_1 = "Bill:   That will cost you $<%= price %> plus taxes, ma'am."
 line_2 = "Pritma: How much is it with taxes?"
 line_3 = "Bill: I don't know! I'm not very good at math."
 
+line_1_erb_instance = ERB.new(line_1)
 puts
-puts line_1
+puts line_1_erb_instance
 puts
 puts line_2
 puts
