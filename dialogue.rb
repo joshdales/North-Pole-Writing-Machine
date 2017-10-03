bb100240fdb8575855196175f1c09181cb884b0f
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 12:51:33 -0400

    set local variable price

--- dialogue.rb
+++ dialogue.rb
@@ -1,3 +1,7 @@
+require 'erb'
+
+price = 8.75
+
 line_1 = "Bill:   That will cost you $8.75 plus taxes, ma'am."
 line_2 = "Pritma: How much is it with taxes?"
 line_3 = "Bill: I don't know! I'm not very good at math."
