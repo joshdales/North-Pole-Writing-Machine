95876600c27958c92e3e34e45dd8698cfb9f4c1e
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 14:28:02 -0400

    naughty_letter_writer writes a naughty letter for each kid

--- naughty_letter_writer.rb
+++ naughty_letter_writer.rb
@@ -1,16 +1,23 @@
 require 'erb'
 
 kids_data   = File.read('data/kids-data.txt')
-nice_letter = File.read('templates/naughty_letter_template.txt.erb')
+naughty_letter = File.read('templates/naughty_letter_template.txt.erb')
 
 kids_data.each_line do |kid|
 
   kid_data_array = kid.split
 
-  name     = kid_data_array[0]
-  gender   = kid_data_array[1]
-  behavior = kid_data_array[2]
-  toys     = kid_data_array[3..8]
+  name       = kid_data_array[0]
+  gender     = kid_data_array[1]
+  behavior   = kid_data_array[2]
+  toys       = kid_data_array[3..8]
+  infraction = kid_data_array[15]
+
+  you_get = toys.shuffle[-1]
+  if you_get == 'Kaleidoscope'
+    you_get = toys.shuffle[-2]
+  end
+  toys.delete(you_get)
 
   next unless behavior == 'naughty'
 
