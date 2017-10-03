d5976486864daa5d50bf2e7d29d13c75a2455e1d
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 12:56:40 -0400

    line_3 uses ERB to calcuate tax

--- dialogue.rb
+++ dialogue.rb
@@ -4,7 +4,7 @@ price = 8.75
 
 line_1 = "Bill:   That will cost you $<%= price %> plus taxes, ma'am."
 line_2 = "Pritma: How much is it with taxes?"
-line_3 = "Bill: I don't know! I'm not very good at math."
+line_3 = "Bill:   That will be $<%= (price * 1.13).round(2) %> please."
 
 
 puts
@@ -12,5 +12,5 @@ puts ERB.new(line_1).result
 puts
 puts line_2
 puts
-puts line_3
+puts ERB.new(line_3).result
 puts
