54205021d9ce56a9aad19fa9623ed8a5d1644944
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 12:50:19 -0400

    Add lines 1-3 in dialogue.rb

---
+++ dialogue.rb
@@ -0,0 +1,11 @@
+line_1 = "Bill:   That will cost you $8.75 plus taxes, ma'am."
+line_2 = "Pritma: How much is it with taxes?"
+line_3 = "Bill: I don't know! I'm not very good at math."
+
+puts
+puts line_1
+puts
+puts line_2
+puts
+puts line_3
+puts
